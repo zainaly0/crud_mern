028ccb6210cb847a01630efe212f30294cb8a419
zainaly0 <zaidk4076@gmail.com>
2024-08-12 02:30:42 +0530

    first commit with install all necessrary packages

---
+++ client/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import './App.css'
+import {BrowserRouter, Routes, Route} from 'react-router-dom'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      
+    </>
+  )
+}
+
+export default App
